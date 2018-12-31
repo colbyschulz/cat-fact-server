7e6605131d1a2b4dbb7b7ab3377169d09d5e83e1
Colby Schulz <colbyschulz@Colbys-MacBook-Pro.local>
2018-12-31 12:20:39 -0500

    allow cors.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+var cors = require("cors");
 const fetch = require("node-fetch");
 
 const app = express();
+app.use(cors());
 const FACT_URL = "https://catfact.ninja/fact";
 const port = process.env.PORT || 8080;
 
