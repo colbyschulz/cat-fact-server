1923a3e4bcdde47c217225482cb5ace185dc9a2f
Colby Schulz <colbyschulz@Colbys-MacBook-Pro.local>
2018-12-31 11:43:24 -0500

    Add call to facts api and return json to / endpoint.

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const fetch = require("node-fetch");
+
+const app = express();
+const FACT_URL = "https://catfact.ninja/fact";
+const port = 8080;
+
+app.get("/", (req, res) => {
+  fetch(FACT_URL)
+    .then(resp => resp.json())
+    .then(r => res.json(r));
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
